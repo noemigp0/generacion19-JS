9f5b377ad684768e5e8e4367c3f1a8cdda73e4fc
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2022-05-26 15:53:31 -0500

    Update homework.js
    Adding homework

--- js/homework.js
+++ js/homework.js
@@ -1,101 +1,69 @@
-//agregar que pueda remover los acentos, y a minusculas
-
-// const isPalindrome = ( str ) => {
-//   str
-//    let auxSinEspacios = '';
-//     for (i = 0; i < str.length; i++) {
-//         if (str[i] !== ' '){
-//             auxSinEspacios += str[i];
-//         }
-       
-//       }
-
-//     let auxStrReversed = "";
-//     for (i = auxSinEspacios.length - 1; i >= 0; i--) {
-//       auxStrReversed += auxSinEspacios[i];
-//     }
-    
-//     console.log(auxSinEspacios );
-//     console.log(auxStrReversed );
-//    return auxSinEspacios === auxStrReversed ? true : false;
-
-// }
-
-
-// let palabra = prompt('Ingresa una palabra').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
-// console.log(isPalindrome(palabra) ? 'Es palindromo' : 'No es palindromo');
-
-
-
 /**
- * Ejercicio 1 *Obligatorio
- * Realizar una funcion que reciba como parametro 1 array
- * Y devuelva un array con solo los elementos Pares de ese array
- * p.ej.
- * -> evenOnly( [1,2,3,4,18] ) -> [2,4,18]
- * -> evenOnly( [10, 2, 5] ) -> [10,2]
- * -> evenOnly() -> 'Se necesita un array'
- * 
+ * Funcion que reciba una palabra
+ * Retorne la palabra invertida
+ *
+ * reverseStr('hola mundo')
+ * -> 'odnum aloh'
+ *
+ * Usar solo metodos de str y array
  */
 
-// const evenOnly = (arreglo = []) => {
-//   console.log(arreglo.length);
-  
-//   if ( arreglo.length > 0 ){
-  
-//   evenOnlyArray = [];
-//   for( i = 0; i <= arreglo.length -1; i++ ){
-//     if( arreglo[i] % 2 == 0 ){
-//       evenOnlyArray.push(arreglo[i]);
-//     }  
-//   }
-//   return evenOnlyArray;
-//  } else {
-//   return 'Se necesita un array';
-//   }  
-// }
+let beforeReverse = "hola mundo";
 
-// //console.log(evenOnly([10, 2, 5]));
-// console.log(evenOnly());
+const reverseStr = (word) => {
+  return word.split("").reverse().toString().replaceAll(",", "");
+};
 
-
+console.log(reverseStr(beforeReverse));
 
 /**
- * Ejercicio 2 *Opcional
- * Funcion que reciba como parametro una array de strings
- * y devuelva el primer y ultimo caracter de cada string
- * p.ej.
- * -> firstAndLast ( ['hola', 'mundo'] ) -> ['ha', 'mo']
+ * Funcion que reciba un array de numeros
+ * y devuelva el promedio de todos los elementos
+ *
+ * getAverage( [10, 8, 9, 7] )
+ * -> 8.5
+ */
+
+const getAverage = (numbersArray = []) => {
+  let auxAverage = 0;
+  numbersArray.forEach((element) => {
+    auxAverage += element;
+  });
+
+  return auxAverage / numbersArray.length;
+};
+
+console.log(getAverage([10, 8, 9, 7]));
+
+/**
+ * Funcion que reciba un array de strings
+ * y devuelva un array indicando si son palindromos o no
+ *
+ * arePalindrome( ['oso','juan', 'seres'] )
+ * -> [true, false, true]
+ */
+
+let isPalindrome = (wordsArray = []) => {
+  let auxArrayReverse = [];
+  wordsArray.forEach((element, index) => {
+    if (
+      element.split("").reverse().toString().replaceAll(",", "") === element
+    ) {
+      auxArrayReverse.push(true);
+    } else {
+      auxArrayReverse.push(false);
+    }
+  });
+
+  return auxArrayReverse;
+};
+
+console.log(isPalindrome(["oso", "juan", "seres"]));
+
+/**
+ * Estudiar los métodos de array
+ * .filter()
+ * .reduce()
  */
 
 
-//para este puedo ocupar  slice
-
-
-//metodo slice puede servir para este ejercicio
-
-
-//Siempre añade elementos al inicio del array
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits);
- fruits.unshift("Lemon", "Pineapple");
-console.log(fruits);
- fruits.unshift("Strawberry", "kiwi");
-console.log(fruits);
-
-
-// //Convierte a cadena separando por comas
-// fruits.toString();
-// console.log(fruits.toString());
-
-
-// //
-
-// console.log(fruits.sort());//ordena de forma ascendente
-// console.log(fruits.reverse());//ordena de forma descendente
-
-
-// //array.slice(start, end) //la ultima posicion no es inclusiva
-// const citrus = fruits.slice(1, 3);
-// console.log(citrus);
