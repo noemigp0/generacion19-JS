a2a7af17a83697948469132e0dcf6cc7796427d2
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2022-06-03 17:09:20 -0500

    Adding dome homework

--- js/homework.js
+++ js/homework.js
@@ -1,13 +1,45 @@
 /**
- * Dado un array de objetos koders
- * Imprimir en consola, todos los nombres de cada koder
- *
- * 'jorge luis Camarillo tiene 30 años y es de la generacion 6'
- * 'Erik Gutierrez tiene 30 años y es de la generacion 16'
- * ....
- *
+ * Ejercicio 1
+ * <ul id="menu" class="menu">
+        <li class="item__menu">Home</li>
+        <li class="item__menu">Products</li>
+        <li class="item__menu">About Us</li>
+    </ul>
+    Replicar con JS este markup
  */
- let koders = [
+
+let menuList = document.createElement("ul");
+let body = document.querySelector("body");
+
+let homeLi = document.createElement("li");
+homeLi.textContent = "Home";
+let productLi = document.createElement("li");
+productLi.textContent = "Products";
+let aboutLi = document.createElement("li");
+aboutLi.textContent = "About Us";
+
+menuList.appendChild(homeLi);
+menuList.appendChild(productLi);
+menuList.appendChild(aboutLi);
+body.appendChild(menuList);
+
+let menu = document.querySelector("ul");
+menu.setAttribute("class", "menu");
+menu.setAttribute("id", "menu");
+let menuOptions = document.querySelectorAll("li");
+
+menuOptions.forEach((cv) => {
+  cv.setAttribute("class", "item__menu");
+});
+
+/**
+ * Dado un arreglo de koders
+ * 1. Generar una lista con la clase koders
+ * 2. Agregar a cada koder en esa lista
+ * 3. A todos los koders agregarles la clase 'item koder'
+ */
+
+let koders = [
   {
     name: "jorge luis",
     lastName: "Camarillo",
@@ -20,7 +52,7 @@
     lastName: "Gutierrez",
     age: 20,
     generation: 15,
-    modulos: ["js"],
+    modulos: ["Node"],
   },
   {
     name: "Sara",
@@ -30,81 +62,34 @@
     modulos: ["js"],
   },
 ];
-const getInfoCoder = (objKoder) => { 
-  let info = ''
-  objKoder.forEach((cv) => {
-    info += `${cv.name} tiene ${cv.age} y es de la generacion ${cv.generation} \n`;
-  });
-  return info
-}
-console.log(getInfoCoder(koders));
 
-const printName = (arrObj) => {//revisar ejemplos de find
-  let info = ''
-  arrObj.find(object => {
-    info += `${object.name} tiene ${object.age} y es de la generacion ${object.generation} \n`;
-    console.log(object.modulos[1]);//para acceder a una propiedad de un objeto con valor tipo arreglo
-  })
-  return info
+let kodersUl = document.createElement("ul");
+kodersUl.setAttribute("class", "koders");
+body.appendChild(kodersUl);
+
+let koderOptions = document.querySelectorAll(".item-koder");
+
+const addTextToLi = (array) => {  
+  array.forEach(element => {
+    let liKoder = kodersUl.appendChild(document.createElement("li")); 
+    liKoder.textContent = `${element.name} ${element.lastName}`;      
+ });
 }
 
-console.log(printName(koders));
+addTextToLi(koders);
+
+
+
 /**
- * Del mismo array de koders
- * Obtener la suma de todas las edades
+ * Opcional
+ * Del mismo arreglo de koders
+ * 1. Generar una tabla de koders con las columnas
+ *   - Nombre completo
+ *   - Edad
+ *   - Generación
+ *
+ *   Opcional -Modulos cursados
+ *
+ * Referencia: https://slack-files.com/TCRFJBKB6-F03J4CBCBA7-8b1c39e7bc
  *
  */
-const getAgeSum = (objKoder) => { 
-  let sumaEdades = 0;
-  objKoder.forEach((cv) => {
-    sumaEdades += cv.age;
-  });
-  return sumaEdades
-}
-console.log(`La suma de todas las edades es ${getAgeSum(koders)}`);
-/**`
- * Del objeto library
- * 1. Obtener el numero de libros que se estan leyendo
- * 2. Obtener una lista de todos los autores
- * 3. Obtener una lista de todos los Libros
- *
- */
-let library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-const getLibraryInfo = (objLibrary) => {
-  let librosLeidos = 0;
-  let listaAutores = [];
-  let listaLibros = [];
-  objLibrary.forEach((cv) => {
-    if (cv.readingStatus === true) {
-      librosLeidos += cv.readingStatus;
-    }
-    listaAutores.push(cv.author);
-    listaLibros.push(cv.title);
-  });
-  console.log(listaAutores);
-  console.log(listaLibros);
-  return `Numero de libros que se estan leyendo: ${librosLeidos} \n lista de todos los autores ${listaAutores}
-  Lista de todos los Libros  ${listaLibros}`;
- }
-console.log(getLibraryInfo(library));
-
-
-
-
-
