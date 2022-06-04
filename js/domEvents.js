038e6340610cf24f845112a0244d749e4ac7fa68
Noemi Gutierrez <noemigp0@gmail.com>
2022-06-03 21:44:42 -0500

    respaldo

---
+++ js/domEvents.js
@@ -0,0 +1,40 @@
+const enviar = () => {
+  console.log("enviar");
+};
+
+const calcular = () => {
+  console.log("calcular");
+};
+
+//Eventos de teclado
+//onkeyup
+
+const empezando = () => {
+    console.log("Empezando a presionar tecla")
+}
+
+const presionando = () => {
+    console.log("presionando tecla")
+}
+
+const terminando = () => {
+    console.log("terminando presionar tecla")
+}
+
+const focoEnInput = () => {
+    console.log("Ahora estas en el input text")
+
+}
+
+//Eventos de navegador
+
+//
+
+
+const seleccionado = () => {
+    console.log("sSeleccionando texto")
+}
+
+const clickDerecho = () =>{
+    console.log("Presionaste clic derecho");
+} 
