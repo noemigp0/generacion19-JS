4404c689f1914bbc7e24f1815f6e9129ec0af227
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2022-06-13 16:47:56 -0500

    adding list koders

--- js/listkoder.js
+++ js/listkoder.js
@@ -1,24 +1,28 @@
 
 
 //const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
-const ajaxXHR = (url, method = 'GET', obj = {} ) => {
-    const xhttp = new XMLHttpRequest()
-    xhttp.open( method, `https://koder19g-ngp-default-rtdb.firebaseio.com/${url}`, true)
-    xhttp.onload = function(data) {
-        if(data.target.status >= 200 && data.target.status <= 399){
-            let response = JSON.parse(data.target.response)
-            console.log(response);
-            //callback(response)
-        }
+const ajaxXHR = (callback, url, method = "GET") => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(
+    method,
+    `https://koder19g-ngp-default-rtdb.firebaseio.com/${url}`,
+    true
+  );
+  xhttp.onload = function (data) {
+    if (data.target.status >= 200 && data.target.status <= 399) {
+      let response = JSON.parse(data.target.response);
+      //console.log(response);
+      callback(response);
     }
-    if(method === 'GET' || method === 'DELETE'){
-        xhttp.send()
-    } else {
-      xhttp.send( JSON.stringify(obj) )
-    }
-  }  
-  
-  /* Estructura de cada koder
+  };
+  if (method === "GET" || method === "DELETE") {
+    xhttp.send();
+  } else {
+    xhttp.send(JSON.stringify(obj));
+  }
+};
+
+/* Estructura de cada koder
     {
       name: 'jorge',
       age: 30,
@@ -27,28 +31,50 @@ const ajaxXHR = (url, method = 'GET', obj = {} ) => {
     }
   */
 
+// uso de la funcion
+// GET , DELETE
+// ajaxXHR(nombreDeFuncionAllamar, `/koders/.json`, 'GET')
+// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'DELETE')
 
+// POST, PUT, PATCH
+// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'POST', obj)
+// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PUT', obj)
+// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PATCH', obj)
 
-//insertKoderInfo(koders)
+// Listar todos los koders
+console.log("SU codigo para editar un koder aquí");
 
+const listKoders = (objKoders) => {
+  let template = "";
+  for (koder in objKoders) {
+    let koderInfo = objKoders[koder];
+    template += `
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${koderInfo.name}</h5>
+                  <div class="row mb-4">
+            <div class="col">
+              <div class="form-outline">
+                 <p class="card-text">Edad: ${koderInfo.age} </p>
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-outline">
+                 <p class="card-text">Bootcamp: ${koderInfo.bootcamp} </p>
+              </div>
+            </div>
+          </div> 
+                <p class="card-text">${koderInfo.biography}</p>                
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>`;
+  }
 
-const listKoderInfo = () => {
+  document.querySelector(".all__posts").innerHTML = template;
+};
 
-}
-
-  
-  // uso de la funcion 
-  // GET , DELETE
-  // ajaxXHR(nombreDeFuncionAllamar, `/koders/.json`, 'GET')
-  // ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'DELETE')
-  
-  // POST, PUT, PATCH
-  // ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'POST', obj)
-  // ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PUT', obj)
-  // ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PATCH', obj)
-  
-  
-  
-  
-  // Listar todos los koders
-  console.log('SU codigo para editar un koder aquí')
+document.addEventListener("DOMContentLoaded", () => {
+  let objKoders = ajaxXHR(listKoders, `/koders/.json`, "GET");
+});
