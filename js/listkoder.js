05e76c98104b06766bddac91cd80b0936ee8a783
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2022-06-15 16:21:35 -0500

    Update listkoder.js

--- js/listkoder.js
+++ js/listkoder.js
@@ -1,59 +1,14 @@
 
 
-//const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
-const ajaxXHR = (callback, url, method = "GET") => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open(
-    method,
-    `https://koder19g-ngp-default-rtdb.firebaseio.com/${url}`,
-    true
-  );
-  xhttp.onload = function (data) {
-    if (data.target.status >= 200 && data.target.status <= 399) {
-      let response = JSON.parse(data.target.response);
-      //console.log(response);
-      callback(response);
-    }
-  };
-  if (method === "GET" || method === "DELETE") {
-    xhttp.send();
-  } else {
-    xhttp.send(JSON.stringify(obj));
-  }
-};
-
-/* Estructura de cada koder
-    {
-      name: 'jorge',
-      age: 30,
-      bootcamp: 'js',
-      biography: 'lorem ipsum dolot sit amet'
-    }
-  */
-
-// uso de la funcion
-// GET , DELETE
-// ajaxXHR(nombreDeFuncionAllamar, `/koders/.json`, 'GET')
-// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'DELETE')
-
-// POST, PUT, PATCH
-// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'POST', obj)
-// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PUT', obj)
-// ajaxXHR(nombreDeFuncionAllamar, `/koders/${idKoder}.json`, 'PATCH', obj)
-
-// Listar todos los koders
 console.log("SU codigo para editar un koder aquí");
-let deleteButton =""
-let deleteButtonValue=""
+
 const listKoders = (objKoders) => {
   let template = "";
   for (koder in objKoders) {
-    
     let koderInfo = objKoders[koder];
     template += `
-    <br>
     <div class="col-sm-6">
-        <div class="card mb-4">
+        <div class="card mb-3">
             <div class="card-body">
                 <h5 class="card-title">${koderInfo.name}</h5>
                   <div class="row mb-4">
@@ -69,57 +24,26 @@ const listKoders = (objKoders) => {
             </div>
           </div> 
                 <p class="card-text">${koderInfo.biography}</p>                
-                <a href="/interior.html?koderkey=${koder}" class="btn btn-link">Ver koder</a>
-                <a href="/updatekoder.html?koderkey=${koder}" class="btn btn-link">Editar koder</a>
-               
+                 <a href="/interior.html?koderkey=${koder}" class="btn btn-link">Ver koder</a>
+                 <a href="/updatekoder.html?koderkey=${koder}" class="btn btn-link">Editar koder</a>
             </div>
         </div>
     </div>`;
   }
 
   document.querySelector(".all__posts").innerHTML = template;
-  
-  // deleteButton = document.querySelector("#delete-btn")
-  // deleteButtonValue = document.querySelector("#delete-btn").value
-  // deleteButton.addEventListener("click", () => {
-  //   console.log(deleteButtonValue);
-  
-  // })
-  
-  
-
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  ajaxXHR(listKoders, `/koders/.json`, "GET");
-});
-
-
-const confirmKoderDeleted = (response) => {
-  console.log(response)
-  if (response === null){
-    alert("Registro eliminado, favor de actualizar", "warning")
-  }
-}
-
-
-//Esta funsion es para los alerts, el cdigo es de bootstrap
-const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-const alert = (message, type) => {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    "</div>",
-  ].join("");
-
-  alertPlaceholder.append(wrapper);
-};
-
-
-const deleteKoder = (idKoder) => {
-  //deleteButtonValue = document.querySelector("#delete-btn").value
-  console.log(idKoder);
-  //ajaxXHR(confirmKoderDeleted, `/koders/${idKoder}.json`, 'DELETE')
-}
+fetch("https://koder19g-ngp-default-rtdb.firebaseio.com/koders/.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}` // si si, lanzamos un error con un mensaje
+      );
+    } else {
+      return response.json(); // sino, retornamos la respuesta al siguiente then
+    }
+  })
+  .then((response) => {
+    listKoders(response);
+  });
