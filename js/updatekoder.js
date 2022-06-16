50df5aae5a7a5d4a692f3582bd25cee8aedf805b
Noemi Gutierrez <noemigp0@gmail.com>
2022-06-15 21:41:52 -0500

    Adding delete button with js

--- js/updatekoder.js
+++ js/updatekoder.js
@@ -1,6 +1,4 @@
-
-console.log('SU codigo para editar un koder aquí')
-
+console.log("SU codigo para editar un koder aquí");
 
 const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 const alert = (message, type) => {
@@ -21,69 +19,95 @@ let age = document.getElementById("age");
 let bootcamp = document.getElementById("bootcamp");
 let biography = document.getElementById("biography");
 
-
-const setInfoKoder = ( response ) => { 
+const setInfoKoder = (response) => {
   console.log(response);
-  name.value = response.name
-  age.value = response.age
-  bootcamp.value = response.bootcamp
-  biography.value = response.biography
-}
+  name.value = response.name;
+  age.value = response.age;
+  bootcamp.value = response.bootcamp;
+  biography.value = response.biography;
+};
 
-const infoKoderUpdated = (response) => { 
-  alert("Registro actualizado", 'success')
-  
-}
+const infoKoderUpdated = (response) => {
+  alert("Registro actualizado", "success");
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  fetch(`https://koder19g-ngp-default-rtdb.firebaseio.com/koders/${idKoder}.json`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(
-        `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}` // si si, lanzamos un error con un mensaje
-      );
-    } else {
-      return response.json(); // sino, retornamos la respuesta al siguiente then
-    }
-  })
-  .then((response) => {
-    setInfoKoder(response);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(
+    `https://koder19g-ngp-default-rtdb.firebaseio.com/koders/${idKoder}.json`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}` // si si, lanzamos un error con un mensaje
+        );
+      } else {
+        return response.json(); // sino, retornamos la respuesta al siguiente then
+      }
+    })
+    .then((response) => {
+      setInfoKoder(response);
+    });
 });
 
 let updateButton = document.getElementById("update");
-updateButton.addEventListener("click", () => { 
+updateButton.addEventListener("click", () => {
   let koderUpdated = {
     name: name.value,
     age: age.value,
     bootcamp: bootcamp.value,
-    biography: biography.value
+    biography: biography.value,
   };
 
-
-  if (name.value === "" || age.value === "" || bootcamp.value === '' || biography.value === '') {
-    alert("Debe llenar todos los campos", 'warning')
+  if (
+    name.value === "" ||
+    age.value === "" ||
+    bootcamp.value === "" ||
+    biography.value === ""
+  ) {
+    alert("Debe llenar todos los campos", "warning");
   } else {
+    fetch(
+      `https://koder19g-ngp-default-rtdb.firebaseio.com/koders/${idKoder}.json`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(koderUpdated),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((finalResponse) => {
+        if (finalResponse) {
+          infoKoderUpdated(finalResponse);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
 
-    fetch ( `https://koder19g-ngp-default-rtdb.firebaseio.com//koders/${idKoder}.json`, {
-    method: 'PATCH',
-    body: JSON.stringify(koderUpdated),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
+//Eliminar koder
+let deleteButton = document.getElementById("delete");
+deleteButton.addEventListener("click", () => {
+  fetch(
+    `https://koder19g-ngp-default-rtdb.firebaseio.com/koders/${idKoder}.json`,
+    {
+      method: "DELETE",
     }
-}).then(response => {
-    return response.json()
-}).then(finalResponse => {
-    if( !finalResponse) {
-     
-    }else {
-      infoKoderUpdated(finalResponse)
-    }
-}).catch( err => {
-    console.log(err)
-})
-  }
-  }
-  
-)
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((finalResponse) => {
+      if (!finalResponse){
+        window.location.pathname = '/index.html'
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
